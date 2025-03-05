bd4d125b3881ada5a45702ecc762a81878e77224
jsndz <jaisondz9360@gmail.com>
2025-03-05 16:43:33 +0530

    structure change

--- project/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import { authconfig } from "@/lib/auth";
-import NextAuth from "next-auth/next";
-
-const handler = NextAuth(authconfig);
-
-export { handler as GET, handler as POST };
--- project/app/api/auth/[...nextauth]/token/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest } from "next/server";
-
-export function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const searchQuery = searchParams.get("Address");
-}
