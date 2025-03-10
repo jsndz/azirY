9cc4b127e753d3c1657d62a5232248dbfdbb0f54
jsndz <jaisondz9360@gmail.com>
2025-03-10 22:57:56 +0530

    swap icon and balance

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -4,16 +4,22 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import PrimaryButton from "./PrimaryButton";
-import { useTokens } from "@/app/api/hooks/useTokens";
+import { tokenwithBalance, useTokens } from "@/app/api/hooks/useTokens";
 import { TokenDetails } from "./TokenDetails";
 import { Swap } from "./Swap";
 
-const Tabs: string[] = ["Assets", "Swap"];
+type Tab = "Assets" | "Swap";
+const Tabs: { id: Tab; name: string }[] = [
+  { id: "Assets", name: "Assets" },
+  { id: "Swap", name: "Swap" },
+];
 
 const Profile = ({ publicKey }: { publicKey: string }) => {
   const session = useSession();
   const router = useRouter();
   const [tab, setTab] = useState("Assets");
+  const { loading, tokenBalance } = useTokens(publicKey);
+
   if (session.status === "loading") {
     return <div className="text-center text-white">Loading...</div>;
   }
@@ -31,20 +37,24 @@ const Profile = ({ publicKey }: { publicKey: string }) => {
         {Tabs.map((tab) => {
           return (
             <button
-              key={tab}
+              key={tab.id}
               className="px-6 py-3 ml-5 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
               onClick={() => {
-                setTab(tab);
+                setTab(tab.id);
               }}
             >
-              {tab}
+              {tab.name}
             </button>
           );
         })}
         {tab == "Assets" ? (
-          <Assets publicKey={publicKey}></Assets>
+          <Assets
+            publicKey={publicKey}
+            loading={loading}
+            tokenBalance={tokenBalance}
+          ></Assets>
         ) : tab == "Swap" ? (
-          <Swap></Swap>
+          <Swap tokenBalance={tokenBalance} loading={loading}></Swap>
         ) : null}
       </div>
     </div>
@@ -60,21 +70,28 @@ const Greeting = ({ name, image }: { name: string; image: string }) => {
   );
 };
 
-function Assets({ publicKey }: { publicKey: string }) {
+function Assets({
+  publicKey,
+  loading,
+  tokenBalance,
+}: {
+  publicKey: string;
+  loading: boolean;
+  tokenBalance: {
+    totalBalance: number;
+    tokens: tokenwithBalance[];
+  } | null;
+}) {
   const [copied, setCopied] = useState(false);
-  const { loading, tokenBalance } = useTokens(publicKey);
 
   if (loading) {
     return <div className="text-center text-gray-400">Loading assets...</div>;
   }
-  console.log(tokenBalance?.tokens);
 
   return (
     <div className="p-4 bg-gray-700 rounded-lg shadow-md">
       <div className="flex justify-between items-center mb-4">
-        <span className="text-sm text-gray-300">Public Key:</span>
         <div className="flex items-center space-x-2">
-          <span className="text-green-400 text-xs break-all">{publicKey}</span>
           <PrimaryButton
             onClick={() => {
               navigator.clipboard.writeText(publicKey);
--- components/Swap.tsx
+++ components/Swap.tsx
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { SUPPORTED_TOKENS, tokenDetails } from "../lib/tokens";
+import { tokenwithBalance } from "@/app/api/hooks/useTokens";
 
-export const Swap = () => {
+export const Swap = ({
+  tokenBalance,
+  loading,
+}: {
+  loading: boolean;
+  tokenBalance: {
+    totalBalance: number;
+    tokens: tokenwithBalance[];
+  } | null;
+}) => {
   const [baseAsset, setBaseAsset] = useState(SUPPORTED_TOKENS[0]);
   const [quoteAsset, setQuoteAsset] = useState(SUPPORTED_TOKENS[1]);
   return (
@@ -12,13 +22,41 @@ export const Swap = () => {
         onSelect={(asset) => {
           setBaseAsset(asset);
         }}
+        subtitle={
+          <div>
+            {`${
+              tokenBalance?.tokens.find((x) => x.name === baseAsset.name)
+                ?.balance
+            } ${baseAsset.name}`}
+          </div>
+        }
       ></SwapInputRow>
+      <div className="flex justify-center">
+        <div
+          onClick={() => {
+            let baseAssetTemp = baseAsset;
+            setBaseAsset(quoteAsset);
+            setQuoteAsset(baseAssetTemp);
+          }}
+          className="cursor-pointer rounded-full w-10 h-10 border absolute mt-[-20px] bg-white flex justify-center pt-2"
+        >
+          <SwapIcon />
+        </div>
+      </div>
       <SwapInputRow
         selectedToken={quoteAsset}
         title="GET"
         onSelect={(asset) => {
           setQuoteAsset(asset);
         }}
+        subtitle={
+          <div>
+            {`${
+              tokenBalance?.tokens.find((x) => x.name === quoteAsset.name)
+                ?.balance
+            } ${quoteAsset.name}`}
+          </div>
+        }
       ></SwapInputRow>
     </div>
   );
@@ -28,14 +66,17 @@ export const SwapInputRow = ({
   onSelect,
   selectedToken,
   title,
+  subtitle,
 }: {
   onSelect: (asset: tokenDetails) => void;
   selectedToken: tokenDetails;
   title: string;
+  subtitle: ReactNode;
 }) => {
   return (
     <div>
       <p>{title}</p>
+      <p>{subtitle}</p>
       <AssetSelector
         selectedToken={selectedToken}
         onSelect={onSelect}
@@ -65,7 +106,11 @@ export const AssetSelector = ({
     >
       {SUPPORTED_TOKENS.map((token) => {
         return (
-          <option key={token.name} value={selectedToken.name}>
+          <option
+            key={token.name}
+            value={token.name}
+            selected={selectedToken.name == token.name}
+          >
             {token.name}
           </option>
         );
@@ -73,4 +118,22 @@ export const AssetSelector = ({
     </select>
   );
 };
-// filter((x) => x.name !== selectedToken.name).
+
+function SwapIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+      />
+    </svg>
+  );
+}
